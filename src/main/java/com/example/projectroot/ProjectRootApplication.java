2ed08898762702fdcddd2b8c7d658602eede8c04
Greg <grygoriy23@gmail.com>
2021-04-19 18:02:44 -0500

    1 part of task

---
+++ src/main/java/com/example/projectroot/ProjectRootApplication.java
@@ -0,0 +1,12 @@
+package com.example.projectroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/projectroot/ProjectRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
